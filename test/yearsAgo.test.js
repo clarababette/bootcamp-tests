064a6758f731b1cda2ff3544d7613c64a45d18ee
clarababette <clara_coetzee@zoho.com>
2021-03-12 15:19:36 +0200

    yearsAgo tests written

---
+++ test/yearsAgo.test.js
@@ -0,0 +1,11 @@
+describe('A function that returns how many years the input year is from the current year.', function(){
+    it('should say that 1994 was 27 years ago. ', function(){
+        assert.equal(yearsAgo(1994), 27);
+    });
+    it('should say that 2020 was 1 year ago. ', function(){
+        assert.equal(yearsAgo(2020), 1);
+    });
+    it('should say that 2021 was zero years ago. ', function(){
+        assert.equal(yearsAgo(2121), 0);
+    })
+});
